2a292e8dd63b61dcd99a445c6217ca2301ebb852
Samar Maurya <95111162+Samar4306@users.noreply.github.com>
2023-12-31 18:23:11 +0530

    Add files via upload

---
+++ config/mongoose.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+// mongoose.set('strictQuery',true);
+// const dotenv = require('.env');
+
+const DB = 'mongodb+srv://HimanshuKatiyar:dyKcTjzl0YfdTiE6@cluster0.dv9w3cp.mongodb.net/?retryWrites=true&w=majority';
+
+// dotenv.config({ path: 'config/.env' });
+
+//connecting mongoose with database
+//I stored MONGODB_URI in my system veriable for security reason. veriable name MONGODB_URI followed by your mongo atlas link
+//for local use you can write this code
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/csvUploader');
+
+// mongoose.connect(DB, {
+// 	useNewUrlParser: true,
+// 	useCreateIndex:true,
+//  	useUnifiedTopology: true,
+// 	useFindAndModify:false
+// }).then(() => {
+// 	console.log('connection successful');
+// }).catch((err) => console.log('no connection',err));
+
+
+mongoose.connect(DB, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Error in connecting to MongoDB'));
+
+db.once('open', function () {
+	console.log('Connected to Database :: Mongodb');
+});
+
+module.exports = mongoose;
